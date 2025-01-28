313e28ed1d788505ea3cded4f565081f3ca12711
TelmoK <telmo.kiss@gmail.com>
2025-01-28 18:41:01 +0100

    DirectoryTree test created

--- Preproduction/File_System_Prototype/DirectoryTree.cpp
+++ Preproduction/File_System_Prototype/DirectoryTree.cpp
@@ -4,15 +4,29 @@
 
 class DirectoryTreeNode
 {
+protected:
 	DirectoryTreeNode* parent;
-	std::string properties;
+	std::string name;
+	std::string root;
 
 public:
 
-	DirectoryTreeNode(DirectoryTreeNode* _parent = nullptr) : parent(_parent) {}
+	DirectoryTreeNode(DirectoryTreeNode* _parent = nullptr, std::string _name = "") : parent(_parent), name(_name)
+	{
+		update_root_path();
+	}
+
 	DirectoryTreeNode* get_parenth() { return parent; }
+	std::string get_root() const { return root; }
+	std::string get_name() const { return name; }
+
+	void update_root_path()
+	{
+		if(parent != nullptr)
+			root = parent->root + "/" + parent->name;
+	}
+
 	virtual DirectoryTreeNode* get_copy() = 0;
-	void update_root_path();
 };
 
 class DirectoryTreeFolderNode : public DirectoryTreeNode
@@ -22,9 +36,48 @@ class DirectoryTreeFolderNode : public DirectoryTreeNode
 
 public:
 
-	DirectoryTreeFolderNode(DirectoryTreeNode* _parent = nullptr) : DirectoryTreeNode(_parent) {}
+	DirectoryTreeFolderNode(DirectoryTreeNode* _parent = nullptr, std::string _name = "New folder") 
+		: DirectoryTreeNode(_parent, _name) {}
 
-	DirectoryTreeNode* get_copy() override {}
+	DirectoryTreeNode* get_copy() override 
+	{
+		DirectoryTreeFolderNode* copy = new DirectoryTreeFolderNode(nullptr, "cpy-"+name);
+
+		std::list<DirectoryTreeNode*> cpy_folder_sons;
+
+		for(DirectoryTreeNode* folder : folders)
+			cpy_folder_sons.push_back(folder->get_copy());
+
+		std::list<DirectoryTreeNode*> cpy_file_sons;
+
+		for(DirectoryTreeNode* file : files)
+			cpy_file_sons.push_back(file->get_copy());
+
+		copy->folders = cpy_folder_sons;
+		copy->files = cpy_file_sons;
+
+		return copy;
+	}
+
+	const std::list<DirectoryTreeNode*>& get_folders() const
+	{
+		return folders;
+	}
+
+	const std::list<DirectoryTreeNode*>& get_files() const
+	{
+		return files;
+	}
+
+	void addFolder(DirectoryTreeNode* folder)
+	{
+		folders.push_back(folder);
+	}
+
+	void addFile(DirectoryTreeNode* file)
+	{
+		files.push_back(file);
+	}
 };
 
 class DirectoryTreeFileNode : public DirectoryTreeNode
@@ -34,13 +87,13 @@ class DirectoryTreeFileNode : public DirectoryTreeNode
 
 public:
 
-	DirectoryTreeFileNode(DirectoryTreeNode* _parent = nullptr, std::string _file_type) 
-		: DirectoryTreeNode(_parent), file_type(_file_type) {}
+	DirectoryTreeFileNode(DirectoryTreeNode* _parent = nullptr, std::string _name = "New File", std::string _file_type = "blank") 
+		: DirectoryTreeNode(_parent, _name), file_type(_file_type) {}
 
-	DirectoryTreeFileNode* get_copy() override
+	DirectoryTreeNode* get_copy() override
 	{
-		DirectoryTreeFileNode* copy = new DirectoryTreeFileNode(nullptr, file_type);
-		copy.file_content = file_content;
+		DirectoryTreeFileNode* copy = new DirectoryTreeFileNode(nullptr, "cpy-"+name, file_type);
+		copy->file_content = file_content;
 
 		return copy;
 	}
@@ -57,10 +110,52 @@ class DirectoryTree
 
 public:
 
-	DirectoryTree(){}
-}
+	DirectoryTree(DirectoryTreeFolderNode* _root = nullptr) : root(_root) {}
+
+	void print(std::ostream& out)
+	{
+		out << root->get_name() << "\n";
+		print_folder(out, root, 1);
+	}
+
+private:
+
+	void print_folder(std::ostream& out, const DirectoryTreeFolderNode* folder, int depth)
+	{
+		std::string spaces = "";
+		for(int i = 0; i < depth; ++i) spaces += "  |";
+
+		for(const DirectoryTreeNode* subfolder : folder->get_folders())
+		{
+			out << spaces << "-" << subfolder->get_name() << "\n";
+			const DirectoryTreeFolderNode* f = dynamic_cast<const DirectoryTreeFolderNode*>(subfolder);
+			print_folder(out, f, depth + 1);
+		}
+
+		for(const DirectoryTreeNode* file : folder->get_files())
+		{
+			out << spaces << "-" << file->get_name() << "\n";
+		}
+	}
+};
 
 int main()
 {
+	DirectoryTreeFolderNode* c = new DirectoryTreeFolderNode(nullptr, "C://");
+	c->addFile(new DirectoryTreeFileNode(c, "file.txt", "txt"));
+	c->addFile(new DirectoryTreeFileNode(c, "file2.txt", "txt"));
+	DirectoryTreeFolderNode* folder = new DirectoryTreeFolderNode(c, "Folder");
+	folder->addFile(new DirectoryTreeFileNode(folder, "file3.txt", "txt"));
+	folder->addFile(new DirectoryTreeFileNode(folder, "file4.txt", "txt"));
 
+	DirectoryTreeFolderNode* folder2 = new DirectoryTreeFolderNode(folder, "Folder 2");
+	folder2->addFile(new DirectoryTreeFileNode(folder2, "file5.txt", "txt"));
+	folder->addFolder(folder2);
+
+	c->addFolder(folder);
+
+	DirectoryTree d_tree(c);
+	d_tree.print(std::cout);
+
+	std::cin.get();
 }
